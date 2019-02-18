0c630760b04341f20e3f77809bb891df7cf518c2
EC2 Default User <ec2-user@ip-172-31-10-237.us-east-2.compute.internal>
2019-02-18 03:15:59 +0000

    <sorting_titles>

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,10 @@
 class Movie < ActiveRecord::Base
-  attr_accessor :comparing_field
-  def <=>(other)
-    if (@comparing_field == "title")
-      return self.title <=> other.title
-    elsif (@comparing_field == "release_date")
-      return self.release_date <=> other.release_date
+  class << self; attr_accessor :comparing_field end
+  def <=>(other_movie)
+    if (@comparing_field == "title_header")
+      return self.title <=> other_movie.title
+    elsif (@comparing_field == "release_date_header")
+      return self.release_date <=> other_movie.release_date
     end
   end
 end
